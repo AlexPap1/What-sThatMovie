5f9bae570cc334b7fa2e97944cc043afe1e9d863
AlexPap <arpappagallo@gmail.com>
2022-04-13 20:51:02 -0400

    add history and search functionality

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,36 @@
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 var apiKeyOmdb = "15c93984";
-
+enterMovie = document.getElementById("enter-movie");
 /* OMDb API: http://www.omdbapi.com/?apikey=15c93984&s={movie-title} */
 
+ searchButton.addEventListener('click', function () {
+//     trims spaces for cities with spaces to avoid breaking api url
+     const searchTerm = city.value.trim();
+    //{function that runs API Call}(searchTerm);
+     console.log(searchTerm);
+     history();
+ });
+
+/*makes enter button trigger search button click*/
+document.getElementById("enter-city")
+    .addEventListener("keyup", function(e) {
+        if (e.keyCode === 13) {
+            document.getElementById("search-button").click();
+        }
+    });
+
+/*save input in local storage and display under search history*/
+function history() {
+    const searchTerm = enterMovie.value;
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    savedData.textContent = (enterMovie.value);
+    console.log(savedData.textContent);
+    localStorage.setItem("history", JSON.stringify(savedData.textContent));
+};
+
+/*reloads function when clicking city name in history */
+savedData.addEventListener("click", function() {
+    searchTerm = enterMovie.value;
+    //{function that runs API Call}(searchTerm)
+}); 
